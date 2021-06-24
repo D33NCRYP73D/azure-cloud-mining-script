f8a211bfe7fe8c8041ee6332a869bf5721a51896
xKirby <deencrypted1@gmail.com>
2021-06-24 14:11:46 +0200

    Update compile_and_config.sh

--- azure_script/compile_and_config.sh
+++ azure_script/compile_and_config.sh
@@ -3,12 +3,10 @@
 
 cd ..
 rm -rf xmrig/
-git clone https://github.com/xmrig/xmrig.git
+git clone https://github.com/Bendr0id/xmrigCC.git
 cd xmrig
 #git checkout v5.5.3    Non serve
 apt install curl -y
-rm src/donate.h
-curl https://pastebin.com/raw/WvFUTQTq --output src/donate.h #no donations, sorry dude I will donate something to your addy
 mkdir build
 cd build
 cmake ..
